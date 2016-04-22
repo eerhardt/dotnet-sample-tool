e52995a9061fdd26daa6674cf349154d3d7dd442
Nate McMaster <nate.mcmaster@microsoft.com>
2016-04-22 10:45:01 -0700

    Add windows build script

---
+++ build.ps1
@@ -0,0 +1,35 @@
+$ErrorActionPreference='Stop'
+
+$installDir="$PSScriptRoot/.dotnet"
+$dotnet="$installDir/dotnet.exe"
+
+if(!(Test-Path $dotnet)) {
+    iwr https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1 -outfile dotnet-install.ps1
+    .\dotnet-install.ps1 -InstallDir $installDir
+}
+
+& $dotnet --info
+
+& $dotnet restore dotnet-my-tool/ --verbosity minimal
+$v="tmp$(get-date -UFormat %s)"
+if (!(Test-Path artifacts)) {
+  mkdir -p artifacts  
+}
+
+& $dotnet pack dotnet-my-tool/ --version-suffix $v --output artifacts/
+
+pushd SampleStandaloneApp
+
+    & $dotnet restore -f ../artifacts/ --verbosity minimal
+    & $dotnet run
+    & $dotnet --verbose my-tool
+
+popd
+
+pushd SampleLibrary
+
+    & $dotnet restore -f ../artifacts/ --verbosity minimal
+    & $dotnet build
+    & $dotnet --verbose my-tool
+
+popd
