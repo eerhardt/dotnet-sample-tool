b39ae7425ff3648ff3280e65102a88c6bca275be
Nate McMaster <nate.mcmaster@microsoft.com>
2016-04-22 10:57:24 -0700

    Make linux build script standalone

--- build.sh
+++ build.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 set -e
+installDir="$(pwd)/.dotnet"
+dotnet="$installDir/dotnet"
 
-dotnet restore dotnet-my-tool/ --verbosity minimal
+if [ ! -e $dotnet ]; then
+    curl https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh | bash -s -- -i $installDir 
+fi
+
+$dotnet --info
+$dotnet restore dotnet-my-tool/ --verbosity minimal
 v="tmp$(date +%s)"
 mkdir -p artifacts/
-dotnet pack dotnet-my-tool/ --version-suffix $v --output artifacts/
+$dotnet pack dotnet-my-tool/ --version-suffix $v --output artifacts/
 
 pushd SampleStandaloneApp
 
-    dotnet restore -f ../artifacts/ --verbosity minimal
-    dotnet run
-    dotnet --verbose my-tool
+    $dotnet restore -f ../artifacts/ --verbosity minimal
+    $dotnet run
+    $dotnet --verbose my-tool
 
 popd
 
 pushd SampleLibrary
 
-    dotnet restore -f ../artifacts/ --verbosity minimal
-    dotnet build
-    dotnet --verbose my-tool
+    $dotnet restore -f ../artifacts/ --verbosity minimal
+    $dotnet build
+    $dotnet --verbose my-tool
 
 popd
