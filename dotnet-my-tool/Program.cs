1f97aab0e21891ce0df625604fc550c1aaced038
Eric Erhardt <eric.erhardt@microsoft.com>
2016-04-25 21:32:39 -0500

    Changing dotnet-my-tool to try all TFMs until it finds one that works. Adding netcoreapp1.0 to the SampleLibrary to allow dotnet-my-tool to work on it.

--- dotnet-my-tool/Program.cs
+++ dotnet-my-tool/Program.cs
@@ -18,28 +18,44 @@ namespace My.Tool
                 Reporter.Output.WriteLine("Called as a tool. Dispatching to dependency command inside project.");
                 var project = ProjectReader.GetProject(Directory.GetCurrentDirectory());
             
-                var framework = project.GetTargetFrameworks().Select(f=>f.FrameworkName).First();
+                foreach(var framework in project.GetTargetFrameworks().Select(f=>f.FrameworkName))
+                {
+                    var projectContext = new ProjectContextBuilder()
+                        .WithProject(project)
+                        .WithTargetFramework(framework)
+                        .Build();
+                    
+                    try
+                    {
+                        int result = new ProjectDependenciesCommandFactory(
+                                projectContext.TargetFramework,
+                                Constants.DefaultConfiguration,
+                                null,
+                                null,
+                                projectContext.ProjectDirectory
+                            )
+                            .Create("dotnet-my-tool", 
+                                    new []{"--dispatch", "--verbose"}, 
+                                    projectContext.TargetFramework, 
+                                    Constants.DefaultConfiguration)
+                            .ForwardStdErr()
+                            .ForwardStdOut()
+                            .Execute()
+                            .ExitCode;
+                        if (result == 0)
+                        {
+                            return result;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Reporter.Output.WriteLine($"Error occurred. Skipping framework '{framework}'");    
+                    }
+                }
                 
-                var projectContext = new ProjectContextBuilder()
-                    .WithProject(project)
-                    .WithTargetFramework(framework)
-                    .Build();
-                
-                return new ProjectDependenciesCommandFactory(
-                        projectContext.TargetFramework,
-                        Constants.DefaultConfiguration,
-                        null,
-                        null,
-                        projectContext.ProjectDirectory
-                    )
-                    .Create("dotnet-my-tool", 
-                            new []{"--dispatch", "--verbose"}, 
-                            projectContext.TargetFramework, 
-                            Constants.DefaultConfiguration)
-                    .ForwardStdErr()
-                    .ForwardStdOut()
-                    .Execute()
-                    .ExitCode;
+                // no TargetFramework worked, error out and tell the user
+                Reporter.Output.WriteLine("Failed to find an appropriate target framework in project '{project}'.");
+                return 1;
             }
             else
             {
